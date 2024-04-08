5c200fdacee3e45a7e22339e95b0a5faad28b4b4
Andre Loubser <ahloubser12@gmail.com>
2024-04-08 21:23:04 +0200

    .

--- apps/cms/src/app/app.component.ts
+++ apps/cms/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ItemManagerComponent } from './Item-manager/Item-manager.component';
+import { ButtonComponent } from '@xtreme-studios/x-components';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ItemManagerComponent],
+  imports: [RouterOutlet, ItemManagerComponent, ButtonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
